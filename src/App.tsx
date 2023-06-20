0d3b996725178b72d76efdd49d4dd63667a1d856
Jhann Rojas <jrojasu@fundaciongruposocial.co>
2023-06-20 07:55:20 -0500

    firrst commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { TextInput } from "flowbite-react"
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline text-red-500">
+        Hello world!
+      </h1>
+      <TextInput />
+    </>
+  )
+}
+
+export default App
